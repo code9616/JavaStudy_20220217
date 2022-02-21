249ef09375cae00dc246473ae54aa3c66623a278
ITPS <ITPS@gmail.com>
2022-02-21 11:15:29 +0900

    두번째 시간 변수에 대한 수업내용

---
+++ JavaStudy_9616/src/a02_변수/StudentInfo.java
@@ -0,0 +1,26 @@
+package a02_변수;
+
+public class StudentInfo {
+
+	public static void main(String[] args) {
+		
+		long studentYear = 21400000000l;
+		int num = 1;
+		double a = 10.5;
+		num = (int)a;
+		a = num;
+		
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+
+	}
+
+}
