787b67f6fd9bdcf77f4916d6c4a0c46c7466de42
ITPS <ITPS@gmail.com>
2022-03-07 12:54:58 +0900

    03-07 클래스 수업

---
+++ JavaStudy_9616/src/a09_클래스2/Tumbler.java
@@ -0,0 +1,20 @@
+package a09_클래스2;
+
+public class Tumbler extends Product{
+	private String cap;  //버튼식, 오픈식
+	
+	public Tumbler(int productCode, String productName, String cap) {
+		super(productCode, productName);
+		this.cap = cap;
+	}
+	
+	public void tumblerTest() {
+		System.out.println("텀블러만 가지고 있는 메소드");
+	}
+	
+	public void showTumblerInfo() {
+		super.showProduct();
+		System.out.println("뚜껑타입: " + cap);
+		System.out.println();
+	}
+}
