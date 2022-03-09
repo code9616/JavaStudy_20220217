0e6e028bb2d9be00443447c79002b4af7bb15733
DESKTOP-0SLK776\82108 <2001lion@naver.com>
2022-03-10 00:29:58 +0900

    인터페이스 수정본

--- JavaStudy_9616/src/a13_인터페이스2/view/Input.java
+++ JavaStudy_9616/src/a13_인터페이스2/view/Input.java
@@ -7,7 +7,7 @@ import a13_인터페이스2.model.User;
 public interface Input {
 	public char typedSelect(Scanner scanner);
 	public User typedUser(Scanner scanner);
-	public String typedUsername(Scanner scanner);
+	public String typedUserName(Scanner scanner);
 
 	
 }
--- JavaStudy_9616/src/a13_인터페이스2/view/InputImpl.java
+++ JavaStudy_9616/src/a13_인터페이스2/view/InputImpl.java
@@ -1,16 +1,18 @@
+
 package a13_인터페이스2.view;
 
 import java.util.Scanner;
 
 import a13_인터페이스2.model.User;
 
-public class InputImpl implements Input{
+public class InputImpl implements Input {
 	@Override
-	public char typedSelect(Scanner scanner) {  // 명령을 선택 입력후 해당 입력값 리턴
-	 System.out.println("명령을 입력하세요: "); 
-	 char select = scanner.next().charAt(0);
-	 scanner.nextLine();
-	 return select;
+	public char typedSelect(Scanner scanner) { //명령을 선택 입력후 해당 입력값 리턴
+		System.out.print(" 명령을 입력하세요: ");
+		char select = scanner.next().charAt(0);
+		scanner.nextLine();
+		return select;
+		
 	}
 	@Override
 	public User typedUser(Scanner scanner) {
@@ -26,13 +28,11 @@ public class InputImpl implements Input{
 		user.setEmail(scanner.nextLine());
 		return user;
 	}
-		
-		@Override
-		public String typedUsername(Scanner scanner) {
-			System.out.println("[아이디로 회원 조회]");
-			System.out.println("회원아이디: ");
-			return scanner.nextLine();
-			
-
+	
+	@Override
+	public String typedUserName(Scanner scanner) {
+		System.out.println("[아이디로 회원 조회]");
+		System.out.println("회원아이디: ");
+		return scanner.nextLine();
 	}
 }
